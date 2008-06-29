5b8d405b1904576af0d62eeda2587fa0030ba911
collin <collintmiller@gmail.com>
2008-06-28 19:44:19 -0500

    made the gem spec "safe"

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,12 @@
+__DIR__ = path = File.dirname(__FILE__)
+
 require 'rubygems'
 require 'spec'
 
 namespace :spec do
   task :prepare do
-    path = File.dirname(__FILE__)
-    p path
-    @specs= Dir.glob("#{path}/rspec/**/*.rb").join(' ')
+    
+    @specs= Dir.glob("#{__DIR__}/rspec/**/*.rb").join(' ')
   end
 
   task :all => :prepare do
@@ -14,5 +15,29 @@ namespace :spec do
   
   task :doc => :prepare do
     system "spec #{@specs} --format specdoc"
+  end
+end
+
+namespace :gem do
+  task :spec do
+    file = File.new("#{__DIR__}/fold.gemspec", 'w+')
+    file.write %{
+Gem::Specification.new do |s|
+  s.name             = "fold"
+  s.version          = "0.0.2"
+  s.platform         = Gem::Platform::RUBY
+  s.has_rdoc         = false
+  s.summary          = "Toolkit for creating whitespace active mini-languages. Inspired by Haml. Feature light."
+  s.description      = s.summary
+  s.author           = "Collin Miller"
+  s.email            = "collintmiller@gmail.com"
+  s.homepage         = "http://github.com/collin/fold"
+  s.require_path     = "lib"
+  s.files            = %w{#{(%w(README Rakefile.rb) + Dir.glob("{lib,rspec}/**/*")).join(' ')}}
+  
+  s.add_dependency  "rake"
+  s.add_dependency  "rspec"
+end
+}
   end
 end
--- fold.gemspec
+++ fold.gemspec
@@ -1,15 +1,16 @@
+
 Gem::Specification.new do |s|
   s.name             = "fold"
-  s.version          = "0.0.1"
+  s.version          = "0.0.2"
   s.platform         = Gem::Platform::RUBY
   s.has_rdoc         = false
-  s.summary          = "   A toolkit for creating whitespace active mini-languages. Inspired by Haml, light on features."
+  s.summary          = "Toolkit for creating whitespace active mini-languages. Inspired by Haml. Feature light."
   s.description      = s.summary
   s.author           = "Collin Miller"
   s.email            = "collintmiller@gmail.com"
   s.homepage         = "http://github.com/collin/fold"
   s.require_path     = "lib"
-  s.files            = %w(README Rakefile.rb) + Dir.glob("{lib,rspec}/**/*")
+  s.files            = %w{README Rakefile.rb lib/fold.rb lib/fold lib/fold/engine.rb lib/fold/abstract_fold.rb lib/fold/fold_factory.rb lib/fold/precompiler.rb rspec/fixtures rspec/fixtures/fold rspec/fixtures/fold/fixture.target.fold rspec/fold_spec.rb rspec/fold_spec_helper.rb rspec/fold rspec/fold/precompiler_spec.rb rspec/fold/fold_factory_spec.rb rspec/fold/engine_spec.rb rspec/fold/abstract_fold_spec.rb}
   
   s.add_dependency  "rake"
   s.add_dependency  "rspec"
