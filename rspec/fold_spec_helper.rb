20ef268d40f386dbbec1db010f08c9bd16123c55
collin <collintmiller@gmail.com>
2008-08-22 18:13:56 -0700

    Wanted to see if I could do something.

--- rspec/fold_spec_helper.rb
+++ rspec/fold_spec_helper.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 __DIR__ = File.dirname(__FILE__)
 $LOAD_PATH << "#{__DIR__}/.." unless $LOAD_PATH.include?("#{__DIR__}/..")
 FoldFixtureRoot= File.expand_path("#{__DIR__}/fixtures/fold")
+
 require "lib/fold"
 require 'pathname'
